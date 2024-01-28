c3eecbfa6e389ed8324fd0002847cb0bd0521bd1
TimurMeshchenko <matsuokisho@gmail.com>
2024-01-29 01:01:28 +0300

    first commit

---
+++ src/main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from contextlib import asynccontextmanager
+from typing import Generator
+from motor.motor_asyncio import AsyncIOMotorClient
+
+# message_collection = None
+
+# @asynccontextmanager
+# async def lifespan(app: FastAPI) -> Generator:  
+#     global message_collection
+    
+#     client = AsyncIOMotorClient("mongodb+srv://matsuokisho:K3XLsuQDyBMUXVHU@cluster0.yxmkutf.mongodb.net/?retryWrites=true&w=majority")
+#     db = client.get_database("messenger")
+#     message_collection = db.get_collection("message")
+
+#     yield
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory=f"static"), name="static")
+app.mount("/media", StaticFiles(directory=f"media"), name="media")
+
+templates = Jinja2Templates(directory="../templates")
+
+client = AsyncIOMotorClient("mongodb+srv://matsuokisho:K3XLsuQDyBMUXVHU@cluster0.yxmkutf.mongodb.net/?retryWrites=true&w=majority")
+db = client.get_database("messenger")
+message_collection = db.get_collection("message")
+
+
+@app.get("/")
+async def main(request: Request):        
+    context = {"request": request}
+
+    return templates.TemplateResponse("main.html", context)
+
+async def get_documents():
+    cursor = message_collection.find()
+    documents = await cursor.to_list(length=None)
+    return documents
+
+
+# create
+# message_collection.insert_one({"abc": "a"})
